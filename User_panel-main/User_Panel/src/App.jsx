4b4c75ec2d9db5edde0bf3ce70a24c467e77b778
Rohan Choudhary <choudharyrohan98269@gmail.com>
2025-11-10 16:46:32 +0530

    Fix case mismatch in User-Panel imports

--- User_panel-main/User_Panel/src/App.jsx
+++ User_panel-main/User_Panel/src/App.jsx
@@ -7,7 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-<<<<<<< HEAD
 import Sidebar from "./Component/Sidebar";
 import TopRightNavbar from "./Component/TopRightNavbar";
 import Dashboard from "./Component/Dashboard";
@@ -33,33 +32,6 @@ import GetSupport from "./component/GetSupport";
 import OpenTicket from "./Component/OpenTicket";
 import ReplyTicket from "./Component/ReplyTicket";
 // import TradeNavbar from "./component/TradeNavbar";
-=======
-import Sidebar from "./component/Sidebar";
-import TopRightNavbar from "./component/TopRightNavbar";
-import Dashboard from "./component/Dashboard";
-import ProfileSetting from "./component/ProfileSetting";
-import ManageOrder from "./component/ManageOrder";
-import TradePage from "./component/TradePage";
-import ManageWallet from "./component/ManageWallet";
-import WalletDetails from "./component/WalletDetails";
-import TwoFactor from "./component/TwoFactor";
-import TradeHistory from "./component/TradeHistory";
-import DepositeHistory from "./component/DepositeHistory";
-import WithdrawHistory from "./component/WithdrawHistory";
-import TransactionHistory from "./component/TransactionHistory";
-import GetSupport from "./component/GetSupport";
-import OpenTicket from "./component/OpenTicket";
-import ReplyTicket from "./component/ReplyTicket";
-import TradeNavbar from "./component/TradeNavbar";
-import TradeLayout from "./component/TradeLayout";
-import MarketOverview from "./component/MarketOverview";
-import CryptoCurrency from "./component/CryptoCurrency";
-import ContactSupport from "../../../src/component/ContactSupport";
-import QuickStart from "../../../src/component/QuickStart";
-import ChangePassword from "./component/ChangePassword";
-// import Logout from "./component/Logout";
-
->>>>>>> f95de5cc7ae398fa15a214d13cb0236ce3e8aa41
 
 function App() {
   const location = useLocation();
